4b99b6260245d916a414d0dce55385ae2b0fe70d
[Luqman <[luqman.bachtiar@orionhealth.com]>
2015-12-24 07:27:25 +1300

    Edited full sed

--- get_prereq.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Update Pi OS
-sudo apt-get update
-sudo apt-get upgrade
-
-# Download and install transmission
-sudo apt-get install transmission-daemon
--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-#### User Preferences ####
-##########################
+#===================================================================================
+# User Preferences ####
 # MAX_JOBS: Maximum number of torrents running
 # EXT_NAME: Name of your external storage device
 # TRANS_USERNAME: Username to login to the web-interface of transmission
@@ -9,82 +8,50 @@
 # EXT_OWNER1: User owning the external drive, typically pi
 # EXT_OWNER2: User owning the external drive, typically pi
 
+#===================================================================================
+
 MAX_JOBS="5";
 EXT_NAME="07E2-7749";
 TRANS_USERNAME="admin";
 TRANS_PASSWORD="admin";
-EXT_OWNER1='USER=pi'
-EXT_OWNER2='User=pi'
+EXT_OWNER='USER=pi';
 
-##########################
+echo '>> Check prerequisites'
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install transmission-daemon
 
-# Check prerequisites
-#./get_prereq.sh
+echo '>> Create torrent download directory in external storage device'
+tdir=/media/pi/${EXT_NAME}/RPi_torrent_downloads/complete;
+DIR_COMPLETE=$tdir;
+tdir=/media/pi/${EXT_NAME}/RPi_torrent_downloads/incomplete;
+DIR_INCOMPLETE=$tdir;
 
-# Create torrent download directory in external storage device
-mkdir -vp /media/pi/${EXT_NAME}/RPi_torrent_downloads/complete
-mkdir -vp /media/pi/${EXT_NAME}/RPi_torrent_downloads/incomplete
-
-DIR_COMPLETE=/media/pi/$EXT_NAME/RPi_torrent_downloads/complete
-DIR_INCOMPLETE=/media/pi/$EXT_NAME/RPi_torrent_downloads/incomplete
-
-echo 'Modify Transmission settings.json file'
-#sudo service transmission-daemon stop
+echo '>> Modify Transmission settings.json file'
 sed -i 's/^.*rpc-whitelist.*$/ "rpc-whitelist": "192.168.*.*"/' /etc/transmission-daemon/settings.json
-
-# Remove lines
-sudo grep -v "download-queue-size" /etc/transmission-daemon/settings.json > temp && mv temp /etc/transmission-daemon/settings.json
-sudo grep -v "download-dir" /etc/transmission-daemon/settings.json > temp && mv temp /etc/transmission-daemon/settings.json
-sudo grep -v "incomplete-dir" /etc/transmission-daemon/settings.json > temp && mv temp /etc/transmission-daemon/settings.json
-sudo grep -v "rpc-password" /etc/transmission-daemon/settings.json > temp && mv temp /etc/transmission-daemon/settings.json
-sudo grep -v "rpc-username" /etc/transmission-daemon/settings.json > temp && mv temp /etc/transmission-daemon/settings.json
-sudo grep -v "rpc-whitelist" /etc/transmission-daemon/settings.json > temp && mv temp /etc/transmission-daemon/settings.json
-sudo grep -v "incomplete-dir-enable" /etc/transmission-daemon/settings.json > temp && mv temp /etc/transmission-daemon/settings.json
-sudo grep -v "{" /etc/transmission-daemon/settings.json > temp && mv temp /etc/transmission-daemon/settings.json
-
-# Add lines with user preferences
-printf -v var '"download-queue-size": %s,' "$MAX_JOBS"
-sudo echo -e "\t$var" | cat - /etc/transmission-daemon/settings.json > temp && mv temp /etc/transmission-daemon/settings.json
-printf -v var '"incomplete-dir-enable": %s,' "true"
-sudo echo -e "\t$var" | cat - /etc/transmission-daemon/settings.json > temp && mv temp /etc/transmission-daemon/settings.json
-printf -v var '"rpc-whitelist": "%s",' "192.168.*.*"
-sudo echo -e "\t$var" | cat - /etc/transmission-daemon/settings.json > temp && mv temp /etc/transmission-daemon/settings.json
-printf -v var '"rpc-username": "%s",' "$TRANS_USERNAME"
-sudo echo -e "\t$var" | cat - /etc/transmission-daemon/settings.json > temp && mv temp /etc/transmission-daemon/settings.json
-printf -v var '"rpc-password": "%s",' "$TRANS_PASSWORD"
-sudo echo -e "\t$var" | cat - /etc/transmission-daemon/settings.json > temp && mv temp /etc/transmission-daemon/settings.json
-printf -v var '"incomplete-dir": "%s",' "$DIR_INCOMPLETE"
-sudo echo -e "\t$var" | cat - /etc/transmission-daemon/settings.json > temp && mv temp /etc/transmission-daemon/settings.json
-printf -v var '"download-dir": "%s",' "$DIR_COMPLETE"
-sudo echo -e "\t$var" | cat - /etc/transmission-daemon/settings.json > temp && mv temp /etc/transmission-daemon/settings.json
-sudo echo '{' | cat - /etc/transmission-daemon/settings.json > temp && mv temp /etc/transmission-daemon/settings.json
-sudo chmod 600  /etc/transmission-daemon/settings.json
-sudo chown -R debian-transmission:debian-transmission /etc/transmission-daemon/settings.json
-sudo rm -f /home/pi/.config/transmission-daemon/settings.json
-sudo ln -sf /etc/transmission-daemon/settings.json /home/pi/.config/transmission-daemon/settings.json
+sed -i 's/^.*download-queue-size.*$/ "download-queue-size": '$MAX_JOBS',/' /etc/transmission-daemon/settings.json
+sed -i 's/^.*download-dir.*$/ "download-dir": '$DIR_COMPLETE',/' /etc/transmission-daemon/settings.json
+sed -i 's/^.*incomplete-dir.*$/ "incomplete-dir": '$DIR_INCOMPLETE',/' /etc/transmission-daemon/settings.json
+sed -i 's/^.*rpc-username.*$/ "rpc-username": '$TRANS_USERNAME',/' /etc/transmission-daemon/settings.json
+sed -i 's/^.*rpc-password.*$/ "rpc-password": '$TRANS_PASSWORD',/' /etc/transmission-daemon/settings.json
+sed -i 's/^.*incomplete-dir-enable.*$/ "incomplete-dir-enable": true,/' /etc/transmission-daemon/settings.json
 sudo service transmission-daemon reload
 sudo service transmission-daemon stop
 
-echo 'Modify Transmission transmission-daemon file'
-sudo grep -v "USER" /etc/init.d/transmission-daemon > temp && mv temp /etc/init.d/transmission-daemon
-sudo awk -v n=14 -v s=$EXT_OWNER1 'NR == n {print s} {print}' /etc/init.d/transmission-daemon > temp && mv temp /etc/init.d/transmission-daemon
+echo '>> Modify Transmission transmission-daemon file'
+sed -i 's/^.*USER=Deb.*$/ '$EXT_OWNER'/' /etc/init.d/transmission-daemon
 
-echo 'Owning some files'
+echo '>> Owning some files'
 sudo chown -R pi:pi /etc/transmission-daemon
 sudo chown -R pi:pi /etc/init.d/transmission-daemon
 sudo chown -R pi:pi /var/lib/transmission-daemon
-sudo chown -R pi:pi /etc/systemd/system/multi-user.target.wants/transmission-daemon.service
 
-echo 'Modify Transmission transmission-daemon.service file'
-sudo grep -v "User" /etc/systemd/system/multi-user.target.wants/transmission-daemon.service > temp && mv temp /etc/systemd/system/multi-user.target.wants/transmission-daemon.service
-sudo awk -v n=6 -v s=$EXT_OWNER2 'NR == n {print s} {print}' /etc/systemd/system/multi-user.target.wants/transmission-daemon.service > temp && mv temp /etc/systemd/system/multi-user.target.wants/transmission-daemon.service
+echo '>> Modify Transmission transmission-daemon.service file'
+sed -i 's/^.*USER=Deb.*$/ '$EXT_OWNER'/' /etc/systemd/system/multi-user.target.wants/transmission-daemon.service
 
-echo 'Extra stuff'
+echo '>> Extra stuff'
 sudo systemctl daemon-reload
-
 sudo mkdir -p /home/pi/.config/transmission-daemon/
 sudo ln -sf /etc/transmission-daemon/settings.json /home/pi/.config/transmission-daemon/
 sudo chown -R pi:pi /home/pi/.config/transmission-daemon/
-
 sudo service transmission-daemon start
-
